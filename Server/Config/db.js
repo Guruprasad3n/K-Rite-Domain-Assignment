c62240dd06ca0cf04f9658749a156913482953d6
Guruprasad3n <gp76652@gmail.com>
2024-03-18 17:01:00 +0530

    updated

--- Server/Config/db.js
+++ Server/Config/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose")
 
 const connectDB = async()=>{
    try{
-    await mongoose.connect(process.env.MONGO_URL)
+    await mongoose.connect(`mongodb+srv://todo:guruabc@cluster0.ao3owst.mongodb.net/dns?retryWrites=true&w=majority&appName=Cluster0`)
     console.log("DB Connected")
    }
    catch(error){
